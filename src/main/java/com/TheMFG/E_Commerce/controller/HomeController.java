1022e522dc3e1702bded63a2cd4ac7a47f074cf5
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-06-26 17:46:30 +0300

    project created

---
+++ src/main/java/com/TheMFG/E_Commerce/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.TheMFG.E_Commerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/products")
+    public String products(){
+        return "product";
+    }
+
+    @GetMapping("/product")
+    public String product(){ return "view_product"; }
+}
+
+
